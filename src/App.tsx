3ca598a75304acd9ae9be856463a36dbdc37919d
Yoo Da Yeon <143688136+daaoooy@users.noreply.github.com>
2025-11-28 02:42:22 +0900

    💄(#10) 디자인 토큰 세팅  (#13)
    * ✨ (#10) 색상 토큰 세팅
    
    * ✨ (#10) 폰트 토큰 세팅
    
    * ✅ (#10) 디자인 토큰 테스트용 컴포넌트 추가
    
    * ✨ (#10) 디자인 토큰 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 function App() {
   return (
     <>
-      <div>테스트 중</div>
+      <div className="flex justify-center items-center h-full gap-5">
+        <div className="bg-boost-blue text-white">색상 토큰 테스트</div>
+        <div className="title1-bold">폰트 토큰 테스트</div>
+      </div>
     </>
   );
 }
